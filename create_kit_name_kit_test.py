b8ee967da2311c60f1563843ae53b07afdff6c7a
PiliVeas <pilar.veas@gmail.com>
2024-11-08 12:21:59 -0300

    create_kit_name_kit_test.py

--- create_kit_name_kit_test.py
+++ create_kit_name_kit_test.py
@@ -1,37 +1,70 @@
-headers = {
-    "Content-Type": "application/json"
-}
+import sender_stand_request
+import data
 
-user_body = {
-    "firstName": "Max",
-    "phone": "+10005553535",
-    "address": "8042 Lancaster Ave.Hamburg, NY"
-}
+#Sustituir 'name' por 'kit_name' en una copia del kit_body:
+def get_kit_body(kit_name):
+    current_kit_body = data.kit_body.copy()
+    current_kit_body['name'] = kit_name
+    return current_kit_body
 
-kit_body = {
-  "name": "Nombre"
-}
+#Pruebas positivas: Preparación
+def positive_assert(kit_name):
+    kit_body = get_kit_body(kit_name)
+    kit_response = sender_stand_request.post_new_client_kit(kit_body)
 
-kit_body_name_t1 = "a"
+    assert kit_response.status_code == 201
+    assert kit_response.json()['name'] == kit_name
 
-kit_body_name_t2 = "Abcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcd" \
-          "abcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcd" \
-          "abcdabcdabcdabcdAbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcd" \
-          "abcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcd" \
-          "abcdabcdabcdabcdabcdabcdabcdabC"
+#Pruebas negativas: Preparación
+def negative_assert(kit_name):
+    kit_body = get_kit_body(kit_name)
+    kit_response = sender_stand_request.post_new_client_kit(kit_body)
 
-kit_body_name_t3 = ""
+    assert kit_response.status_code == 400
 
-kit_body_name_t4 = "Abcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcd" \
-            "abcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcd" \
-            "abcdabcdabcdabcdAbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcd" \
-            "abcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcd" \
-            "abcdabcdabcdabcdabcdabcdabcdabcD"
+#Prueba 8: Preparación
+def negative_assert_no_name(kit_body):
+    response = sender_stand_request.post_new_client_kit(kit_body)
 
-kit_body_name_t5 = ""№%@","
+    assert response.status_code == 400
 
-kit_body_name_t6 = " A Aaa "
+#Prueba 1. Número min permitido de caracteres (1):
+def test_create_kit_1_letter_in_name_get_success_response():
+    positive_assert(data.kit_body_name_t1)
 
-kit_body_name_t7 = "123"
+#Prueba 2. Número max permitido de caracteres (511):
+def test_create_kit_511_letter_in_name_get_success_response():
+    positive_assert(data.kit_body_name_t2)
 
-kit_body_name_t9 = 123
+#Prueba 3. Número de caracteres menor que la cantidad permitida (0):
+def test_create_kit_empty_name_get_error_response():
+    negative_assert(data.kit_body_name_t3)
+
+#Prueba 4. Número de caracteres mayor que la cantidad permitida (512):
+def test_create_kit_512_letter_in_name_get_error_response():
+    negative_assert(data.kit_body_name_t4)
+
+#Prueba 5. Caracteres especiales permitidos:
+def test_create_kit_has_special_symbol_in_name_get_success_response():
+    positive_assert(data.kit_body_name_t5)
+
+#Prueba 6. Espacios:
+def test_create_kit_has_space_in_name_get_success_response():
+    positive_assert(data.kit_body_name_t6)
+
+#Prueba 7. Números:
+def test_create_kit_has_number_in_name_get_success_response():
+    positive_assert(data.kit_body_name_t7)
+
+#Prueba 8. Sin parámetro:
+def test_create_kit_no_name_get_error_response():
+    kit_body = data.kit_body.copy()
+    kit_body.pop('name')
+    negative_assert_no_name(kit_body)
+
+#Prueba 9. Parámetro diferente (número):
+def test_create_kit_number_type_name_get_error_response():
+    kit_body = get_kit_body(data.kit_body_name_t9)
+    response = sender_stand_request.post_new_client_kit(kit_body)
+
+    assert response.status_code == 400
